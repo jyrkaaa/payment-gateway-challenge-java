7b49e09ab6fe35dd7f546edaef73e800ed3dee81
Jürgen Kuusik <jyrka55@gmail.com>
2026-08-05 14:38:35 +0300

    4xx not retired

--- src/main/java/com/checkout/payment/gateway/client/IBankClient.java
+++ src/main/java/com/checkout/payment/gateway/client/IBankClient.java
@@ -5,8 +5,8 @@ public interface IBankClient {
     /**
      * Submits a payment to the acquiring bank.
      *
-     * @throws com.checkout.payment.gateway.exception.BankServiceUnavailableException on 503 or network failure
-     * @throws com.checkout.payment.gateway.exception.BankCommunicationException on unexpected errors
+     * @throws com.checkout.payment.gateway.exception.BankServiceUnavailableException on 5xx or network failure
+     * @throws com.checkout.payment.gateway.exception.BankCommunicationException on 4xx or other unexpected errors
      */
     BankPaymentResponse sendPayment(BankPaymentRequest request);
 }
